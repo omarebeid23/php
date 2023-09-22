b27e8283d80ff35d4747cf8c751f7a984e663cf7
omarebeid23 <127976679+omarebeid23@users.noreply.github.com>
2023-09-22 17:43:40 +0300

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Blog</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+      <!-- owl stylesheets -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css" rel="stylesheet" />
+   </head>
+   <body>
+      <!-- header section start -->
+      <div class="header_section">
+         <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+               <a class="logo" href="#"><img src="images/logo.png"></a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                     <li class="nav-item active">
+                     <a class="nav-link" href="index.php">Home</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="services.php">Services</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="about.php">About US</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="blog.php">Blog</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact US</a>
+                     </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                     <div class="login_menu">
+                        <ul>
+                           <li><a href="#">Login</a></li>
+                           <li><a href="#"><img src="images/user-icon.png"></a></li>
+                           <li><a href="#"><img src="images/trolly-icon.png"></a></li>
+                           <li><a href="#"><img src="images/search-icon.png"></a></li>
+                        </ul>
+                     </div>
+                  </form>
+               </div>
+            </nav>
+         </div>
+      </div>
+      <!-- header section end -->
+      <!-- Blog section start -->
+      <div class="blog_section layout_padding">
+         <div class="container">
+            <h1 class="blog_taital">Latest Blog</h1>
+            <div class="blog_section_2">
+               <div class="row">
+                  <div class="col-md-5">
+                     <div class="face_img"><img src="images/face-img.png" class="face_img"></div>
+                  </div>
+                  <div class="col-md-7">
+                     <h1 class="face_text">Face Cream Very mosurations</h1>
+                     <p class="lorem_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                     <div class="read_bt_1"><a href="#">Read More</a></div>
+                  </div>
+               </div>
+            </div>
+            <div class="blog_section_3">
+               <div class="row">
+                  <div class="col-md-7">
+                     <h1 class="face_text">Face Cream Very mosurations Skin</h1>
+                     <p class="lorem_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                     <div class="readmore_bt"><a href="#">Read More</a></div>
+                  </div>
+                  <div class="col-md-5">
+                     <div class="face_img"><img src="images/face-img1.png" class="face_img"></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- Blog section end -->
+      ontact section end -->
+      <!-- footer section start -->
+      <div class="footer_section layout_padding">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-3 col-md-6">
+                  <div class="location_icon">
+                     <ul>
+                        <li><a href="https://www.google.com/maps"><img src="images/map-icon.png"></a></li>
+                        <li><a href="https://mail.google.com/mail/u/0/"><img src="images/mail-icon.png"></a></li>
+                        <li><a href=""><img src="images/call-icon.png"></a></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-lg-6 col-md-6">
+                  <div class="mail_box">
+                     <textarea class="enter_email_text" placeholder="Enter Your Email" rows="5" id="comment" name="Message"></textarea>
+                     <div class="subscribe_bt_1"><a href="#">Subscribe</a></div>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-6">
+                  <div class="social_icon">
+                     <ul>
+                        <li><a href="https://www.facebook.com/"><img src="images/fb-icon.png"></a></li>
+                        <li><a href="https://twitter.com/"><img src="images/twitter-icon.png"></a></li>
+                        <li><a href="https://www.linkedin.com/"><img src="images/linkedin-icon.png"></a></li>
+                        <li><a href="https://www.instagram.com/"><img src="images/instagram-icon.png"></a></li>
+                        <li><a href="https://www.youtube.com/"><img src="images/youtub-icon.png"></a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="footer_section_2">
+               <div class="row">
+                  <div class="col-lg-3 col-md-6">
+                     <h3 class="company_text">Club</h3>
+                     <p class="dolor_text">In our club you will feel just like being at home, we offer lots of actvities and we bring joy to our members. </p>
+                  </div>
+                  <div class="col-lg-3 col-md-6">
+                     <h3 class="company_text">Sports</h3>
+                     <p class="dolor_text">We offer lost of activities and sports in our club, you will find lots of sports that you have the joy to join. </p>
+                  </div>
+                  <div class="col-lg-3 col-md-6">
+                     <h3 class="company_text">Support</h3>
+                     <p class="dolor_text">The special thing about our community that whenever you are lost you will find support around the 24/7 of your day. </p>
+                  </div>
+                  <div class="col-lg-3 col-md-6">
+                     <h3 class="company_text">MY ACCOUNT</h3>
+                     <p class="dolor_text">Hurry up and subscribe for a membership in our community and come and join our society for more exclusive offers. </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- footer section end -->
+      <!-- copyright section start -->
+      <div class="copyright_section">
+         <div class="container">
+            <p class="copyright_text">© 2020 All Rights Reserved.</p>
+         </div>
+      </div>
+      <!-- copyright section end  -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <!-- owl carousel -->
+      <script>
+         $('.owl-carousel').owlCarousel({
+         loop:true,
+         margin:30,
+         nav:true,
+         responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:4
+          }
+         }
+         })
+      </script>
+   </body>
+</html>
---
+++ index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Home-Squad</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+      <!-- owl stylesheets -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css" rel="stylesheet" />
+   </head>
+   <body>
+      <!-- header section start -->
+      <div class="header_section">
+         <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+               <a class="logo" href="#"><img src="images/logo4.png"></a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                     <li class="nav-item active">
+                     <a class="nav-link" href="index.php">Home</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="services.php">Services</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="about.php">About US</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="blog.php">Blog</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact US</a>
+                     </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                     <div class="login_menu">
+                        <ul>
+                           <li><a href="signup.php">Login</a></li>
+                           <li><a href="signup.php"><img src="images/user-icon.png"></a></li>
+            
+                           <li><a href="#"><img src="images/search-icon.png"></a></li>
+                        </ul>
+                     </div>
+                  </form>
+               </div>
+            </nav>
+         </div>
+      </div>
+      <!-- header section end -->
+      <!-- banner section start -->
+      <div class="banner_section banner_bg">
+         <div class="container-fluid">
+            <div id="my_slider" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <div class="taital_main">
+                        <div class="taital_left">
+                           <h1 class="banner_taital">Deni Product For Skin</h1>
+                           <div class="read_bt"><a href="#">Read More</a></div>
+                        </div>
+                        <div class="taital_right">
+                        <!---->   <div class="product_img"><img src="images/swimming.jpeg"></div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="taital_main">
+                        <div class="taital_left">
+                           <h1 class="banner_taital">Deni Product For Skin</h1>
+                           <div class="read_bt"><a href="#">Read More</a></div>
+                        </div>
+                        <div class="taital_right">
+                           <div class="product_img"><img src="images/football.jpg.webp"></div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="taital_main">
+                        <div class="taital_left">
+                           <h1 class="banner_taital">Deni Product For Skin</h1>
+                           <div class="read_bt"><a href="#">Read More</a></div>
+                        </div>
+                        <div class="taital_right">
+                           <div class="product_img"><img src="images/basketball.jpeg"></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <a class="carousel-control-prev" href="#my_slider" role="button" data-slide="prev">
+               <i class='fa fa-arrow-up'></i>
+               </a>
+               <a class="carousel-control-next" href="#my_slider" role="button" data-slide="next">
+               <i class='fa fa-arrow-down'></i>
+               </a>
+            </div>
+         </div>
+      </div>
+      <!-- banner section end -->
+      <!-- about section start -->
+      
+      <!-- about section end -->
+      <!-- product section start -->
+      <div class="product_section layout_padding">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <h1 class="product_taital">Sports</h1>
+                  <p class="product_text">Here you can find the suitable sport for you.</p>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="owl-carousel owl-theme">
+                     <div class="item">
+                        <div class="image_main"><img src="images/Layer5.png" alt="image" /></div>
+                        <h6 class="price_text">Basketball</h6>
+                     </div>
+                     <div class="item">
+                        <div class="image_main"><img src="images/Layer6.png" alt="image" /></div>
+                        <h6 class="price_text">Judo </h6>
+                     </div>
+                     <div class="item">
+                        <div class="image_main"><img src="images/Layer7.png" alt="image" /></div>
+                        <h6 class="price_text">Swimming </h6>
+                     </div>
+                     <div class="item">
+                        <div class="image_main"><img src="images/Layer8.png" alt="image" /></div>
+                        <h6 class="price_text">Tennis </h6>
+                     </div>
+                     <div class="item">
+                        <div class="image_main"><img src="images/Layer9.png" alt="image" /></div>
+                        <h6 class="price_text">Handball </h6>
+                     </div>
+                     
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- product section start -->
+      <!-- client section start -->
+   
+      <!-- Blog section end -->
+      <!-- contact section start -->
+     
+         <div class="container-fluid">
+            <h1 class="contact_taital">Feel free to send us a Message</h1>
+            <div class="contact_section_2">
+               <div class="row">
+                  <div class="col-md-6">
+                     <div class="image_7"><img src="images/academy.jpg"></div>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="mail_section_1">
+                        <input type="text" class="mail_text" placeholder="Your Name" name="Your Name">
+                        <input type="text" class="mail_text" placeholder="Phone Number" name="Phone Number">
+                        <input type="text" class="mail_text" placeholder="Email" name="Email">
+                        <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment" name="Massage"></textarea>
+                        <div class="send_bt"><a href="#">SEND</a></div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- contact section end -->
+      <!-- footer section start -->
+      <div class="footer_section layout_padding">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-3 col-md-6">
+                  <div class="location_icon">
+                     <ul>
+                        <li><a href="https://www.google.com/maps"><img src="images/map-icon.png"></a></li>
+                        <li><a href="https://mail.google.com/mail/u/0/"><img src="images/mail-icon.png"></a></li>
+                        <li><a href=""><img src="images/call-icon.png"></a></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-lg-6 col-md-6">
+                  <div class="mail_box">
+                     <textarea class="enter_email_text" placeholder="Enter Your Email" rows="5" id="comment" name="Message"></textarea>
+                     <div class="subscribe_bt_1"><a href="#">Subscribe</a></div>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-6">
+                  <div class="social_icon">
+                     <ul>
+                        <li><a href="https://www.facebook.com/"><img src="images/fb-icon.png"></a></li>
+                        <li><a href="https://twitter.com/"><img src="images/twitter-icon.png"></a></li>
+                        <li><a href="https://www.linkedin.com/"><img src="images/linkedin-icon.png"></a></li>
+                        <li><a href="https://www.instagram.com/"><img src="images/instagram-icon.png"></a></li>
+                        <li><a href="https://www.youtube.com/"><img src="images/youtub-icon.png"></a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="footer_section_2">
+               <div class="row">
+                  <div class="col-lg-3 col-md-6">
+                     <h3 class="company_text">Club</h3>
+                     <p class="dolor_text">In our club you will feel just like being at home, we offer lots of actvities and we bring joy to our members. </p>
+                  </div>
+                  <div class="col-lg-3 col-md-6">
+                     <h3 class="company_text">Sports</h3>
+                     <p class="dolor_text">We offer lost of activities and sports in our club, you will find lots of sports that you have the joy to join. </p>
+                  </div>
+                  <div class="col-lg-3 col-md-6">
+                     <h3 class="company_text">Support</h3>
+                     <p class="dolor_text">The special thing about our community that whenever you are lost you will find support around the 24/7 of your day. </p>
+                  </div>
+                  <div class="col-lg-3 col-md-6">
+                     <h3 class="company_text">MY ACCOUNT</h3>
+                     <p class="dolor_text">Hurry up and subscribe for a membership in our community and come and join our society for more exclusive offers. </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- footer section end -->
+      <!-- copyright section start -->
+      <div class="copyright_section">
+         <div class="container">
+            <p class="copyright_text">© 2020 All Rights Reserved.</p>
+         </div>
+      </div>
+      <!-- copyright section end  -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <!-- owl carousel -->
+      <script>
+         $('.owl-carousel').owlCarousel({
+         loop:true,
+         margin:30,
+         nav:true,
+         responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:4
+          }
+         }
+         })
+      </script>
+   </body>
+</html>
